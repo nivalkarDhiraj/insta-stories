cc3568ed74533e04f59ea80598060befa3451191
nivalkarDhiraj <nivalkardhiraj@gmail.com>
2021-08-13 09:23:22 +0530

    added lupload limit to test

--- server.js
+++ server.js
@@ -8,6 +8,18 @@ const storyRouter = require("./routes/story");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+var bodyParser = require('body-parser')
+
+app.use(bodyParser.json({
+    limit: '50mb'
+}));
+
+app.use(bodyParser.urlencoded({
+    limit: '50mb',
+    paramaterLimit: 100000,
+    extended: true
+}));
+
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cors());
